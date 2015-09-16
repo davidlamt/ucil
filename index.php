cc916701ae2af19cc489376d8c778d6dafb37364
David Tran <davidlamt@gmail.com>
2015-09-15 19:11:46 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>UCIL: UCI Living</title>
+    
+    <!-- Linking CSS dependencies -->
+    <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="resources/css/creative.css">
+    
+    <!-- Linking JS dependencies -->
+    <script type="text/javascript" src="resources/js/creative.js"></script>
+</head>
+<body>
+   <div id="home-page">
+        <header>
+            <div class="container">
+                <h1>UCIL</h1> 
+                <h2>Helping UCI Students Find Housing</h2>               
+            </div>
+        </header>       
+   </div>
+</body>
+</html>
