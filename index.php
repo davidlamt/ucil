bda71eb752cbd6c8604deca784f51baa7c22ca4f
David Tran <davidlamt@gmail.com>
2015-09-15 19:32:49 -0700

    Added icons and grid

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     
     <!-- Linking CSS dependencies -->
     <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" type="text/css" href="vendors/css/grid.css">
+    <link rel="stylesheet" type="text/css" href="vendors/css/font-awesome.min.css">
     <link rel="stylesheet" type="text/css" href="resources/css/creative.css">
     
     <!-- Linking JS dependencies -->
@@ -18,7 +20,28 @@
                 <h1>UCIL</h1> 
                 <h2>Helping UCI Students Find Housing</h2>               
             </div>
-        </header>       
+        </header>
+        
+        <section id="main-section">
+            <div class="container">
+                <div class="section group">
+                    <div class="col span_1_of_2">
+                        <i class="fa fa-home fa-2x icon"></i>
+                        <h2>Find Apartments</h2>
+                    </div>
+                    <div class="col span_1_of_2">
+                        <i class="fa fa-plus fa-2x icon"></i>
+                        <h2>Add Listing</h2>
+                    </div>
+                </div>
+            </div>
+        </section>    
+        
+        <footer>
+            <div class="container">
+                <p>&copy; 2015 &mdash; A project by <a href="http://davidtranscend.com/" target="_blank">David Tran</a></p>
+            </div>
+        </footer>  
    </div>
 </body>
 </html>
